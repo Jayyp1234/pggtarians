04024702cf380642858a9b002db04dcdff701064
vickk <justusofnigeria@gmail.com>
2022-06-06 05:31:20 +0100

    db added

---
+++ pggtarians.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2022 at 06:28 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pgg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pggtarians`
+--
+
+CREATE TABLE `pggtarians` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `dob` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pggtarians`
+--
+
+INSERT INTO `pggtarians` (`id`, `fullname`, `email`, `username`, `phone_number`, `dob`, `password`) VALUES
+(2, 'Okeleye  Eric Oladipupo', 'okeleyeEric@gmail.com', 'Experience', '09021020816', '-1-12-2024', 'Expericne01</>'),
+(4, 'Kenzy Ellah', 'kenzyela@kg.com', 'kenzy', '09060099140', '01-01-1999', 'Kenzy01@');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pggtarians`
+--
+ALTER TABLE `pggtarians`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pggtarians`
+--
+ALTER TABLE `pggtarians`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
